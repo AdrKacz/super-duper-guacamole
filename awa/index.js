b6e96848c71104d38a3a49ab5045a9a248dceecd
AdrKacz <adrien.kaczmarek@gmail.com>
2021-10-18 20:34:56 +0200

    Setup AWS Amplify Notifications

--- awa/index.js
+++ awa/index.js
@@ -6,6 +6,8 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import Amplify from 'aws-amplify';
+import PushNotification from '@aws-amplify/pushnotification';
+import { PushNotificationIOS } from '@react-native-community/push-notification-ios';
 import config from './src/aws-exports';
 Amplify.configure(config);
 
