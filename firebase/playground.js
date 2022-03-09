170470a8b9e24b3a8353a097282f2ce1bdcf71e0
AdrKacz <adrien.kaczmarek@gmail.com>
2022-03-09 09:43:30 +0100

    Update gitignore and remove cache - Godot

--- firebase/playground.js
+++
@@ -1,3 +0,0 @@
-const { sendMessageDebug } = require('./src/firebase');
-
-sendMessageDebug('fBpHq4jxgU2OoXQQ8Ijuxm:APA91bEnvGxFuFHuYBgZosp1SUYpLZh-tb23RRrD0FVUsmqEShnafA0jM1uITvRQxz2iZvMX3NeBXdQGjzlmnfmjBWUzIZjejhgLTW-D4VmUNKY-8SWPm1qQVUTB33jqG766r1knM62Q');
