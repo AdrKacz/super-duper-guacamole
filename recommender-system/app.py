ab86d599cf2d96187c61e7a7faadec5eac3f4446
MaxiBo <79444086+mbeaufre@users.noreply.github.com>
2022-03-16 18:35:57 +0100

    working lambda with HTTP requests

--- recommender-system/app.py
+++ recommender-system/app.py
@@ -2,11 +2,12 @@ import boto3
 import numpy as np
 import json
 import os
+from urllib import parse
 
 # Get the service resource.
-dynamodb = boto3.resource('dynamodb')
+client_dynamodb = boto3.resource('dynamodb')
 # Access the desired table resource
-db_model_y = dynamodb.Table('awa-model-y')
+table_model_y = client_dynamodb.Table('awa-model-y')
 # Define the client to interact with AWS Lambda
 client_lambda = boto3.client('lambda')
 
@@ -16,39 +17,45 @@ N_USERS = int(os.environ.get('N_USERS'))
 ARN_LAMBDA_X = os.environ.get('ARN_LAMBDA_X')
 assert K_VAL != None and N_USERS != None and ARN_LAMBDA_X != None
 
-def get_matrix_y(db, key_name, key_value, attribute_value):
+def get_matrix_y(table, key_name, key_value, attribute_value):
     """Function to get the Y matrix from the database"""
-    response = db.get_item(Key={key_name: key_value})
+    response = table.get_item(Key={key_name: key_value})
     item = response['Item']
     return np.array(json.loads(item[attribute_value]))
 
-def update_y_matrix(db, key_name, key_value, matrix_y_update):
+def update_y_matrix(table, key_name, key_value, matrix_y_update):
     """Function to update the Y matrix in the database - 1st version"""
-    db.update_item(Key={key_name: key_value},
+    table.update_item(Key={key_name: key_value},
     UpdateExpression='SET matrix = :matrix_update',
     ExpressionAttributeValues={':matrix_update': json.dumps(matrix_y_update.tolist(), separators=(',', ':'), sort_keys=True, indent=4)})
 
 def handler(event, context):
     print("Event: ", event)
-    update_y_bool = bool(event['update'])
-    user_id = event["user_id"]
-    try:
-        assert type(user_id)==str and type(update_y_bool) == bool
-    except:
-        return "ERROR 203"
-    matrix_y_update = np.random.rand(K_VAL, N_USERS)
+    # event = json.loads(event)
     # Variables
-    user_id = event["user_id"] # 1
+    update_y_bool = True    #bool(event['update'])
+    try:
+        print(event["rawQueryString"])
+        url_parsed = parse.parse_qs(event["rawQueryString"])
+        print(url_parsed)
+        user_id = "".join(url_parsed["user_id"])
+        assert type(int(user_id))==int and type(update_y_bool) == bool
+    except:
+        return {'statusCode': '400',
+            'body': "Wrong data types"}
+
+    matrix_y_update = np.random.rand(K_VAL, N_USERS)
 
     if update_y_bool:
-        update_y_matrix(db_model_y, 'user_id', 'matrix_y', matrix_y_update)
+        update_y_matrix(table_model_y, 'user_id', 'matrix_y', matrix_y_update)
 
     #### Get item matrix_y from the table (the Y matrix)
-    y_matrix = get_matrix_y(db_model_y, 'user_id', 'matrix_y', 'matrix')
+    y_matrix = get_matrix_y(table_model_y, 'user_id', 'matrix_y', 'matrix')
     try:
         assert isinstance(y_matrix, np.ndarray) and y_matrix.shape == (K_VAL, N_USERS)
     except:
-        return "ERROR 204"
+        return {'statusCode': '400',
+            'body': "Wrong model"}
 
     ### Interaction with the AWS lambda of the x-part of the model (users)
     # Define the input parameters that will be passed on to the model x function
@@ -72,8 +79,30 @@ def handler(event, context):
     inference_x = np.array(x_json['inference_x'])
     print(inference_x.shape)
 
-    return json.dumps({'user_id' : user_id_x,
-            'inference_x' : inference_x.tolist()},
-             separators=(',', ':'), sort_keys=True, indent=4)
+    return {'statusCode': response['ResponseMetadata']['HTTPStatusCode'],
+            'body': json.dumps(inference_x.tolist())}
+
+    # return json.dumps({'user_id' : user_id_x,
+    #         'inference_x' : inference_x.tolist()},
+    #          separators=(',', ':'), sort_keys=True, indent=4)
+
+    # json.dumps({
+    # "isBase64Encoded": False,
+    # "statusCode": 200,
+    # "body": "Hello from Lambda!",
+    # "headers": {
+    #     "content-type": "application/json"
+    # }
+    # })
+
+##### Output format of a Lambda function for proxy integration
+# {
+#     "isBase64Encoded": true|false,
+#     "statusCode": httpStatusCode,
+#     "headers": { "headerName": "headerValue", ... },
+#     "multiValueHeaders": { "headerName": ["headerValue", "headerValue2", ...], ... },
+#     "body": "..."
+# }
+
 
 ###### Connexion between 2 LAMBDAS : Need to json.dumps() the dictonnary you're sending, but no need to json.loads to get it in the other lambda !!
--- recommender-system/model-x/app.py
+++ recommender-system/model-x/app.py
@@ -3,16 +3,16 @@ import numpy as np
 import json
 
 # Get the service resource.
-dynamodb = boto3.resource('dynamodb')
+client_dynamodb = boto3.resource('dynamodb')
 # Access the desired table resource
-db_model_x = dynamodb.Table('awa-model-x-users-simul')
+table_model_x = client_dynamodb.Table('awa-model-x-users-simul')
 
-def create_new_user(db, key_name, key, k, random=True):
-    db.put_item(Item={key_name: str(key),
+def create_new_user(table, key_name, key, k, random=True):
+    table.put_item(Item={key_name: str(key),
     'x_u': json.dumps(np.random.rand(k).tolist(), separators=(',', ':'), sort_keys=True, indent=4)})
 
-def get_xu_vector(db, user_id):
-    response = db.get_item(Key={'user_id': str(user_id)})
+def get_xu_vector(table, user_id):
+    response = table.get_item(Key={'user_id': str(user_id)})
     item = response['Item']
     x_u = np.array(json.loads(item['x_u']))
     x_u = x_u[:,np.newaxis]
@@ -20,9 +20,8 @@ def get_xu_vector(db, user_id):
 
 def handler(event, context):
     print("Event: ", event)
-    # Environment variable
-    K_VAL = event["K_VAL"]  # Int
     # Variables
+    K_VAL = event["K_VAL"]  # Int
     user_id = str(event["user_id"]) # str
     y_matrix = np.array(event["y_matrix"])
     print("Type :", type(user_id), type(K_VAL))
@@ -30,21 +29,26 @@ def handler(event, context):
         assert type(user_id) == str and type(K_VAL) == int 
         assert isinstance(y_matrix, np.ndarray) and y_matrix.shape[0] == K_VAL
     except:
-        return "ERROR 204"
+        return {'statusCode': '400',
+            'body': "Wrong data type"}
 
     print("Y_matrix :", type(y_matrix), y_matrix.shape) 
 
     # Check if it is a new user:
-    response = db_model_x.get_item(Key={'user_id': str(user_id)})
+    response = table_model_x.get_item(Key={'user_id': str(user_id)})
     print(response)
     ### User creation
     if 'Item' not in response:
         # Matrix initialisation
-        create_new_user(db_model_x, 'user_id', user_id, K_VAL)
+        create_new_user(table_model_x, 'user_id', user_id, K_VAL)
 
-    x_u = get_xu_vector(db_model_x, user_id) # Vector in a matrix shape (1,k)
+    x_u = get_xu_vector(table_model_x, user_id) # Vector in a matrix shape (1,k)
     print("x_u :", x_u.shape)
-    assert isinstance(x_u, np.ndarray) and x_u.shape == (K_VAL,1)
+    try:
+        assert x_u.shape == (K_VAL,1)
+    except:
+        return {'statusCode': '400',
+            'body': "Wrong data shape"}
 
     ### Inference
     # 1st step : Check is the matrix computation is possible
@@ -52,14 +56,19 @@ def handler(event, context):
     alpha = 0.5
     lambda_reg = 0.5
     # r_ui = np.dot(x_u.T, y_matrix[:,i])
-    r_u = np.dot(x_u.T, y_matrix) # (1,K)x(K,N) = # (1,N)
-    # p_ui = int(bool(r_ui != 0.0))
-    # p_u = r_u
+    r_u = np.dot(x_u.T, y_matrix) # (1,K)x(K,N) = # (1,N)   # Note supposé être >0, prendre valeur absolue ?
+    # p_ui = r_ui != 0.0
+    p_u = r_u!=0.0 # (1,N)
+    print("Shape Y : ", Y.shape)
+    print("Shape r_u : ", r_u.shape)
+    print("Shape p_u : ", p_u.shape)
     # c_ui = 1 + alpha*r_ui
     c_u = 1 + alpha * r_u
-    C_u_diag = np.diag(c_u)
+    print("Shape c_u : ", c_u.shape)
+    C_u_diag = np.diag(c_u.tolist())
+    print("Shape C_u_diag : ", C_u_diag.shape)
     # x_u_opt = np.dot(np.linalg.inv(Y @ C_u_diag @ Y.T + lambda_reg*np.identity(K_VAL)), Y @ C_u_diag @ p_u)
-
+    # print("Shape x_u_diag : ", x_u_opt.shape)
 
     ### Gradient computation
 
