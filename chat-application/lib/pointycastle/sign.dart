2550e346e5d62769a2601e62e8e90e813a38718b
AdrKacz <adrien.kaczmarek@gmail.com>
2022-05-27 18:07:31 +0100

    Add sign method

---
+++ chat-application/lib/pointycastle/sign.dart
@@ -0,0 +1,48 @@
+import 'dart:typed_data';
+// ignore: implementation_imports
+import 'package:pointycastle/src/platform_check/platform_check.dart';
+import "package:pointycastle/export.dart";
+
+AsymmetricKeyPair<RSAPublicKey, RSAPrivateKey> generateRSAkeyPair(
+    SecureRandom secureRandom,
+    {int bitLength = 2048}) {
+  // Create an RSA key generator and initialize it
+
+  // final keyGen = KeyGenerator('RSA'); // Get using registry
+  final keyGen = RSAKeyGenerator();
+
+  keyGen.init(ParametersWithRandom(
+      RSAKeyGeneratorParameters(BigInt.parse('65537'), bitLength, 64),
+      secureRandom));
+
+  // Use the generator
+
+  final pair = keyGen.generateKeyPair();
+
+  // Cast the generated key pair into the RSA key types
+
+  final myPublic = pair.publicKey as RSAPublicKey;
+  final myPrivate = pair.privateKey as RSAPrivateKey;
+
+  return AsymmetricKeyPair<RSAPublicKey, RSAPrivateKey>(myPublic, myPrivate);
+}
+
+SecureRandom exampleSecureRandom() {
+  final secureRandom = SecureRandom('Fortuna')
+    ..seed(
+        KeyParameter(Platform.instance.platformEntropySource().getBytes(32)));
+  return secureRandom;
+}
+
+// See Uint8List.fromList(plainText.codeUnits)
+Uint8List rsaSign(RSAPrivateKey privateKey, Uint8List dataToSign) {
+  //final signer = Signer('SHA-256/RSA'); // Get using registry
+  final signer = RSASigner(SHA256Digest(), '0609608648016503040201');
+
+  // initialize with true, which means sign
+  signer.init(true, PrivateKeyParameter<RSAPrivateKey>(privateKey));
+
+  final sig = signer.generateSignature(dataToSign);
+
+  return sig.bytes;
+}
