503a849bb1928575314c351b65d254a8320d12f7
AdrKacz <adrien.kaczmarek@gmail.com>
2022-06-12 18:03:12 +0100

    update mail address

--- chat-application/lib/message.dart
+++ chat-application/lib/message.dart
@@ -157,7 +157,7 @@ Future<void> mailToReportMessage(
 
   final String mailto = Uri(
     scheme: 'mailto',
-    path: 'thunder_parsons.09@icloud.com',
+    path: 'awachat.app@gmail.com',
     queryParameters: {
       'subject': 'Signalement',
       'body': """--- --- ---
