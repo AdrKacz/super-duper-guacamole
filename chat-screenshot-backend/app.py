b01bb2e020e80c56197bf56ff64ebcb964845ab1
AdrKacz <adrien.kaczmarek@gmail.com>
2022-06-12 17:43:44 +0100

    send simple conversation

--- chat-screenshot-backend/app.py
+++ chat-screenshot-backend/app.py
@@ -3,12 +3,80 @@
 import asyncio
 import websockets
 import json
+from datetime import datetime
+from math import floor
 
+message_count = 0
+def encodeMessage(author, created_at_isoformat, text):
+    global message_count
+    message_count += 1
+
+    created_at_datetime = datetime.fromisoformat(created_at_isoformat)
+    return f"{author}::{floor(created_at_datetime.timestamp() * 1000)}::text-{message_count}::{text}"
+
+seen_ids = set()
 async def register(websocket):
+    global seen_ids
     message = await websocket.recv()
     print(">>>", message)
 
-    answer = json.dumps({"action":"register","unreadData":[]})
+    body = json.loads(message)
+    user_id = body['id']
+    if (user_id in seen_ids):
+        answer = json.dumps({
+        'action':'register',
+        'group': 'group-screenshot',
+        'unreadData':[],
+        })
+        print("<<<", answer)
+        await websocket.send(answer)
+        await asyncio.Future()
+    seen_ids.add(user_id)
+
+    answer = json.dumps({
+        'action':'register',
+        'group': 'group-screenshot',
+        'unreadData':[
+            {
+                'action': 'joingroup',
+                'groupid': 'group-screenshot',
+                'users': {
+                    user_id: {
+                        'id': user_id,
+                        'isActive': True
+                    },
+                    'user-01': {
+                        'id': 'user-01',
+                        'isActive': True
+                    },
+                    'user-02': {
+                        'id': 'user-02',
+                        'isActive': False
+                    },
+                    'user-03': {
+                        'id': 'user-03',
+                        'isActive': True
+                    },
+                }
+            },
+            {
+                'action': 'textmessage',
+                'message': encodeMessage('user-01', '2022-06-10 16:20:00+01:00', 'Bonjour')
+            },
+            {
+                'action': 'textmessage',
+                'message': encodeMessage('user-02', '2022-06-10 16:25:00+01:00', "Hello")
+            },
+            {
+                'action': 'textmessage',
+                'message': encodeMessage('user-01', '2022-06-10 16:28:00+01:00', "Hola")
+            },
+            {
+                'action': 'textmessage',
+                'message': encodeMessage(user_id, '2022-06-10 16:29:00+01:00', "Hey")
+            }
+        ]
+    })
     print("<<<", answer)
     await websocket.send(answer)
     await asyncio.Future()
