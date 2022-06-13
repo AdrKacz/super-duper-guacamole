d033ef9702d6830341ed39a179f55706d8f0bd2b
AdrKacz <adrien.kaczmarek@gmail.com>
2022-06-13 07:53:02 +0100

    clean code

--- chat-screenshot-backend/app.py
+++ chat-screenshot-backend/app.py
@@ -1,87 +1,110 @@
 #!./chat-screenshot-backend/.venv/bin/python3.9
 
-import asyncio
-import websockets
+"""
+Mock the real backend architecture
+
+Send a pre-defined series of messages.
+Don't react to any input from the client.
+"""
+
 import json
 from datetime import datetime
 from math import floor
 from uuid import uuid4
+import asyncio
 
-message_count = 0
-def encodeMessage(author, created_at_isoformat, text):
-    global message_count
-    message_count += 1
+import websockets
+
+MESSAGE_COUNT = 0
+
+
+def encode_message(author, created_at_isoformat, text):
+    """
+    Encode message so it can be read by the app
+        author : String - author id
+        create_at_isoformat : String - datetime of the message in isoformat
+        text : String - text of the message
+    """
+    global MESSAGE_COUNT
+    MESSAGE_COUNT += 1
 
     created_at_datetime = datetime.fromisoformat(created_at_isoformat)
-    return f"{author}::{floor(created_at_datetime.timestamp() * 1000)}::text-{message_count}::{text}"
+    return f"{author}::{floor(created_at_datetime.timestamp() * 1000)}::\
+text-{MESSAGE_COUNT}::{text}"
 
-seen_ids = set()
-snapshot = {}
+
+SEEN_IDS = set()
+SNAPSHOT = {}
+
+
 async def register(websocket):
-    global seen_ids, snapshot
+    """
+    Receive register action and reply the series of messages
+        websocket - connection automatically sent by main
+    """
+    global SEEN_IDS, SNAPSHOT
     message = await websocket.recv()
 
     body = json.loads(message)
-    user_id = body['id']
+    user_id = body["id"]
     print(f"Receive register from {user_id}")
-    if (user_id in seen_ids):
-        answer = json.dumps({
-        'action':'register',
-        'group': 'group-screenshot',
-        'unreadData':[],
-        })
+    if user_id in SEEN_IDS:
+        answer = json.dumps(
+            {
+                "action": "register",
+                "group": "group-screenshot",
+                "unreadData": [],
+            }
+        )
         print(f"User {user_id} already seen")
         await websocket.send(answer)
         await asyncio.Future()
-    seen_ids.add(user_id)
+    SEEN_IDS.add(user_id)
 
-    unreadData = list()
+    unread_data = []
     # create users
-    users = {user_id: {
-        'id': user_id,
-        'isActive': True
-    }}
-    usersMap = list()
-    for status in snapshot['isActiveUsers']:
-        id = str(uuid4())
-        users[id] = {
-            'id': id,
-            'isActive': status
-        }
-        usersMap.append(id)
+    users = {user_id: {"id": user_id, "isActive": True}}
+    users_map = []
+    for status in SNAPSHOT["isActiveUsers"]:
+        other_user_id = str(uuid4())
+        users[other_user_id] = {"id": other_user_id, "isActive": status}
+        users_map.append(other_user_id)
 
-    unreadData.append({
-                'action': 'joingroup',
-                'groupid': 'group-screenshot',
-                'users': users
-    })
+    unread_data.append(
+        {"action": "joingroup", "groupid": "group-screenshot", "users": users}
+    )
     # create messages
-    for message in snapshot['messages']:
-        if message['author'] == 0:
+    for message in SNAPSHOT["messages"]:
+        if message["author"] == 0:
             author = user_id
         else:
-            author = usersMap[message['author'] - 1]
-        unreadData.append({
-            'action': 'textmessage',
-            'message': encodeMessage(author, message['datetime'], message['text'])
-        })
+            author = users_map[message["author"] - 1]
+        unread_data.append(
+            {
+                "action": "textmessage",
+                "message": encode_message(author, message["datetime"], message["text"]),
+            }
+        )
 
-    answer = json.dumps({
-        'action':'register',
-        'group': 'group-screenshot',
-        'unreadData': unreadData
-    })
+    answer = json.dumps(
+        {"action": "register", "group": "group-screenshot", "unreadData": unread_data}
+    )
     print(f"Send conversation to {user_id}")
     await websocket.send(answer)
     await asyncio.Future()
 
+
 async def main():
-    print(f"Run websocket at ws://localhost:8765")
+    """
+    Start the websocket server
+    """
+    print("Run websocket at ws://localhost:8765")
     async with websockets.serve(register, "localhost", 8765):
         await asyncio.Future()  # run forever
 
+
 if __name__ == "__main__":
     # get instruction
-    with open('snapshot.json') as f:
-        snapshot = json.load(f)
+    with open("snapshot.json", "r", encoding="utf8") as f:
+        SNAPSHOT = json.load(f)
     asyncio.run(main())
