14a008ee280ceb8730e1db4f9d12b6a1b1b97b64
AdrKacz <adrien.kaczmarek@gmail.com>
2022-06-20 19:05:45 +0100

    skip missing imports

--- chat-backend/actions/switchgroup/app.js
+++ chat-backend/actions/switchgroup/app.js
@@ -13,10 +13,10 @@
 
 // ===== ==== ====
 // IMPORTS
-const { DynamoDBClient } = require('@aws-sdk/client-dynamodb')
-const { DynamoDBDocumentClient, QueryCommand } = require('@aws-sdk/lib-dynamodb')
+const { DynamoDBClient } = require('@aws-sdk/client-dynamodb') // skipcq: JS-0260
+const { DynamoDBDocumentClient, QueryCommand } = require('@aws-sdk/lib-dynamodb') // skipcq: JS-0260
 
-const { SNSClient, PublishCommand } = require('@aws-sdk/client-sns')
+const { SNSClient, PublishCommand } = require('@aws-sdk/client-sns') // skipcq: JS-0260
 
 // ===== ==== ====
 // CONSTANTS
