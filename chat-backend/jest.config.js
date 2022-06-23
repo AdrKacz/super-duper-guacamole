69cf66f16cf3c695a1534b15a200b817e05c65cc
AdrKacz <adrien.kaczmarek@gmail.com>
2022-06-23 21:08:21 +0100

    update jest config

--- chat-backend/jest.config.js
+++ chat-backend/jest.config.js
@@ -20,7 +20,12 @@ module.exports = {
   collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  // collectCoverageFrom: undefined,
+  collectCoverageFrom: [
+    '**/*.js',
+    '!**/*.config.js',
+    '!**/node_modules/**',
+    '!**/vendor/**'
+  ],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
@@ -32,15 +37,12 @@ module.exports = {
   ],
 
   // Indicates which provider should be used to instrument code for coverage
-  coverageProvider: 'v8'
+  coverageProvider: 'v8',
 
   // A list of reporter names that Jest uses when writing coverage reports
-  // coverageReporters: [
-  //   "json",
-  //   "text",
-  //   "lcov",
-  //   "clover"
-  // ],
+  coverageReporters: [
+    ['text', { skipFull: true }]
+  ]
 
   // An object that configures minimum threshold enforcement for coverage results
   // coverageThreshold: undefined,
