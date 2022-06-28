4afda3d1f86af5fbcd11b321626b821fc91d1ead
AdrKacz <adrien.kaczmarek@gmail.com>
2022-06-28 17:57:54 +0100

    add unit test

--- chat-backend/actions/register/__tests__/app.test.js
+++ chat-backend/actions/register/__tests__/app.test.js
@@ -9,7 +9,7 @@ const { SNSClient } = require('@aws-sdk/client-sns')
 const ddbMock = mockClient(DynamoDBDocumentClient)
 const snsMock = mockClient(SNSClient)
 
-function generateIdentity () {
+function generateIdentity (timestamp) {
   const { privateKey, publicKey } = generateKeyPairSync('rsa', {
     modulusLength: 2048,
     publicKeyEncoding: {
@@ -22,7 +22,9 @@ function generateIdentity () {
     }
   })
   const id = '12345'
-  const timestamp = Date.now()
+  if (typeof timestamp !== 'number') {
+    timestamp = Date.now()
+  }
   const signer = createSign('rsa-sha256')
   signer.update(id + timestamp.toString())
   const signature = signer.sign(privateKey, 'base64')
@@ -52,38 +54,51 @@ test('it rejects empty body', async () => {
   await expect(handler(event)).rejects.toThrow('id, signature, timestamp, and publicKey must be defined')
 })
 
-test('it rejects body without id', async () => {
-  const { signature, timestamp } = generateIdentity()
-  const event = {
+test('it rejects body without id, signature, timestamp, and publicKey', async () => {
+  const { id, signature, timestamp, publicKey } = generateIdentity()
+
+  // no id
+  await expect(handler({
     requestContext: {
       connectionId: '012345678'
     },
-    body: JSON.stringify({ signature, timestamp })
-  }
+    body: JSON.stringify({ signature, timestamp, publicKey })
+  })).rejects.toThrow('id, signature, timestamp, and publicKey must be defined')
 
-  await expect(handler(event)).rejects.toThrow('id, signature, timestamp, and publicKey must be defined')
+  // no signature
+  await expect(handler({
+    requestContext: {
+      connectionId: '012345678'
+    },
+    body: JSON.stringify({ id, timestamp, publicKey })
+  })).rejects.toThrow('id, signature, timestamp, and publicKey must be defined')
+
+  // no timestamp
+  await expect(handler({
+    requestContext: {
+      connectionId: '012345678'
+    },
+    body: JSON.stringify({ id, signature, publicKey })
+  })).rejects.toThrow('id, signature, timestamp, and publicKey must be defined')
+
+  // no public key
+  await expect(handler({
+    requestContext: {
+      connectionId: '012345678'
+    },
+    body: JSON.stringify({ id, signature, timestamp })
+  })).rejects.toThrow('id, signature, timestamp, and publicKey must be defined')
 })
 
-test('it rejects body without signature', async () => {
-  const { id, timestamp } = generateIdentity()
-  const event = {
+test('it rejects body with old timestamp', async () => {
+  const { id, signature, timestamp, publicKey } = generateIdentity(Date.now() - 5000)
+
+  const response = await handler({
     requestContext: {
       connectionId: '012345678'
     },
-    body: JSON.stringify({ id, timestamp })
-  }
+    body: JSON.stringify({ id, signature, timestamp, publicKey })
+  })
 
-  await expect(handler(event)).rejects.toThrow('id, signature, timestamp, and publicKey must be defined')
-})
-
-test('it rejects body without timestamp', async () => {
-  const { id, signature } = generateIdentity()
-  const event = {
-    requestContext: {
-      connectionId: '012345678'
-    },
-    body: JSON.stringify({ id, signature })
-  }
-
-  await expect(handler(event)).rejects.toThrow('id, signature, timestamp, and publicKey must be defined')
+  expect(response).toStrictEqual({ statusCode: 401 })
 })
