dcd916b16ad238e5d4319d08dc7f475db6ccea47
AdrKacz <61620013+AdrKacz@users.noreply.github.com>
2022-09-18 17:44:50 +0100

    ci: enforce code coverage with CodeCov and read all JS folder with DeepSource
    * Add script to verify Deepsource config
    
    * ci: add workflow
    
    * Format code with black and standardjs
    
    * ci: sort array before compare
    
    * ci: update replace expression
    
    * ci: update sanitization
    
    * ci: update replace regexp
    
    * doc: add documentation to scripts
    
    * ci: update replace
    
    * ci: Add CodeCoverage on JS
    
    * ci: update jest config to write coverage
    
    * ci: force new code to be fully tested
    
    Co-authored-by: deepsource-autofix[bot] <62050782+deepsource-autofix[bot]@users.noreply.github.com>

---
+++ scripts/verify-deepsource-config/main.js
@@ -0,0 +1,47 @@
+const util = require('node:util')
+const exec = util.promisify(require('node:child_process').exec)
+const toml = require('toml')
+const fs = require('fs')
+
+/**
+ * Get Javascript Analyzer in DeepSource config file
+ */
+function getJavascriptAnalyzer () {
+  const deepSourceConfigToml = fs.readFileSync('../.deepsource.toml', 'utf8')
+  const deepSourceConfig = toml.parse(deepSourceConfigToml)
+  for (const analyzer of deepSourceConfig.analyzers) {
+    if (analyzer.name === 'javascript') {
+      return analyzer
+    }
+  }
+
+  throw new Error('javascript analyzer not found')
+}
+
+/**
+ * Veirfy DeepSource config file has all dependencies for Javascript
+ */
+async function verifyDeepsourceConfig () {
+  // Find all package.json
+  const { stdout, stderr } = await exec('find .. -name "node_modules" -prune -o -name ".aws-sam" -prune -o -name "package.json"')
+  console.error('stderr:', stderr)
+
+  let stdoutArray = stdout.split('\n')
+  stdoutArray = stdoutArray.filter((path) => path.endsWith('/package.json'))
+  stdoutArray = stdoutArray.map((path) => path.replace(/\.\.\//, ''))
+  stdoutArray = stdoutArray.map((path) => path.replace(/package.json/, ''))
+  stdoutArray.sort()
+
+  const stringStdoutArray = `${stdoutArray.map((path) => `"${path}"`).join(',\n')},`
+  console.log('correct set of dependencies:\n', stringStdoutArray, '\n')
+
+  const deepSourceConfigToml = fs.readFileSync('../.deepsource.toml', 'utf8')
+  const deepSourceConfig = toml.parse(deepSourceConfigToml)
+  const javascriptAnalyzer = getJavascriptAnalyzer(deepSourceConfig)
+  javascriptAnalyzer.meta.dependency_file_paths.sort()
+
+  if (stdoutArray.toString() !== javascriptAnalyzer.meta.dependency_file_paths.toString()) {
+    throw new Error("dependency_file_paths doesn't include every dependencies")
+  }
+}
+verifyDeepsourceConfig()
