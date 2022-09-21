b80ad4584f96524af981e4809af4bbe90fa96ae2
AdrKacz <adrien.kaczmarek@gmail.com>
2022-09-21 18:27:38 +0100

    test: update tests description

--- chat-backend/actions/banrequest/__tests__/app.test.js
+++ chat-backend/actions/banrequest/__tests__/app.test.js
@@ -52,12 +52,12 @@ test('it reads environment variables', () => {
 
 describe('getUserFromConnectionId', () => {
   test.each([
-    { details: 'nothing', items: [], expected: {} },
-    { details: 'empty object', items: [{}], expected: {} },
-    { details: 'two users', items: [{ id: dummyUserId }, { id: `${dummyUserId}-2` }], expected: { id: dummyUserId, groupId: undefined } },
-    { details: 'group defined and groupId not defined', items: [{ id: dummyUserId, group: dummyGroupId }], expected: { id: dummyUserId, groupId: dummyGroupId } },
-    { details: 'group and groupId defined', items: [{ id: dummyUserId, groupId: dummyGroupId, group: `${dummyGroupId}-2` }], expected: { id: dummyUserId, groupId: dummyGroupId } }
-  ])('.fetch $details', async ({ items, expected }) => {
+    { details: 'it returns an empty object if no user found', items: [], expected: {} },
+    { details: 'it returns an empty object if user has no id', items: [{}], expected: {} },
+    { details: 'it returns the first associated user', items: [{ id: dummyUserId }, { id: `${dummyUserId}-2` }], expected: { id: dummyUserId, groupId: undefined } },
+    { details: 'it returns user with group attribute if groupId attribute is not defined', items: [{ id: dummyUserId, group: dummyGroupId }], expected: { id: dummyUserId, groupId: dummyGroupId } },
+    { details: 'it returns user with groupId attribute over group attribute', items: [{ id: dummyUserId, groupId: dummyGroupId, group: `${dummyGroupId}-2` }], expected: { id: dummyUserId, groupId: dummyGroupId } }
+  ])('.test $details', async ({ items, expected }) => {
     ddbMock.on(QueryCommand, {
       TableName: process.env.USERS_TABLE_NAME,
       IndexName: process.env.USERS_CONNECTION_ID_INDEX_NAME,
