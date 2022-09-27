58389cc22dd0b75245be775e4f4f86e5851e76bf
AdrKacz <adrien.kaczmarek@gmail.com>
2022-09-27 17:51:55 +0100

    fix: split textmessage function in separate files

---
+++ chat-backend/actions/textmessage/src/get-group-users.js
@@ -0,0 +1,76 @@
+// ===== ==== ====
+// IMPORTS
+const { GetCommand, BatchGetCommand } = require('@aws-sdk/lib-dynamodb')
+
+const { dynamoDBDocumentClient } = require('../aws-clients')
+
+const {
+  USERS_TABLE_NAME,
+  GROUPS_TABLE_NAME
+} = process.env
+
+// ===== ==== ====
+// EXPORTS
+/**
+ * Get group of users
+ *
+ * @param {string} groupId
+ * @param {Object[]?} fetchedUsers - list of users already fetched
+ * @param {Set<string>?} forbiddenUserIds - don't send message to these users
+ *
+ * @return {Promise<{id: string, connectionId: string}[]>} - list of users just fetched concatenated with users already fetched
+ */
+exports.getGroupUsers = async ({ groupId, fetchedUsers, forbiddenUserIds }) => {
+  console.log('getGroupUsers', groupId, fetchedUsers, forbiddenUserIds)
+  // get group user ids
+  const getGroupCommand = new GetCommand({
+    TableName: GROUPS_TABLE_NAME,
+    Key: { id: groupId },
+    ProjectionExpression: '#id, #users',
+    ExpressionAttributeNames: {
+      '#id': 'id',
+      '#users': 'users'
+    }
+  })
+  const group = await dynamoDBDocumentClient.send(getGroupCommand).then((response) => (response.Item))
+
+  if (typeof group === 'undefined') {
+    throw new Error(`group (${groupId}) is not defined`)
+  }
+
+  // define set of users to fetch
+  const fetchedUserIds = new Set([])
+  for (const fetchedUserId of (fetchedUsers ?? [])) {
+    fetchedUserIds.add(fetchedUserId.id)
+  }
+
+  const groupUserIds = []
+  for (const groupUserId of group.users) {
+    if (!(forbiddenUserIds ?? new Set()).has(groupUserId) || !fetchedUserIds.has(groupUserId)) {
+      groupUserIds.push({ id: groupUserId })
+    }
+  }
+
+  if (groupUserIds.length === 0) {
+    throw new Error('cannot send message to no one')
+  }
+
+  // get users
+  const batchGetUsersCommand = new BatchGetCommand({
+    RequestItems: {
+      [USERS_TABLE_NAME]: {
+        Keys: groupUserIds,
+        ProjectionExpression: '#id, #connectionId',
+        ExpressionAttributeNames: {
+          '#id': 'id',
+          '#connectionId': 'connectionId'
+        }
+      }
+    }
+  })
+
+  const users = await dynamoDBDocumentClient.send(batchGetUsersCommand).then((response) => (response.Responses[USERS_TABLE_NAME]))
+
+  console.log('getGroupUsers - return')
+  return users.concat(fetchedUsers ?? [])
+}
---
+++ chat-backend/actions/textmessage/src/send-message-to-group.js
@@ -0,0 +1,55 @@
+// ===== ==== ====
+// IMPORTS
+const { PublishCommand } = require('@aws-sdk/client-sns')
+
+const { snsClient } = require('../aws-clients')
+
+const { getGroupUsers } = require('./get-group-users')
+
+const {
+  SEND_MESSAGE_TOPIC_ARN,
+  SEND_NOTIFICATION_TOPIC_ARN
+} = process.env
+
+// ===== ==== ====
+// EXPORTS
+/**
+ * Send message to a group of users
+ *
+ * @param {string} groupId
+ * @param {Object} message - message to send
+ * @param {Object?} notification - notification to send if any
+ * @param {Object[]?} fetchedUsers - list of users already fetched
+ * @param {Set<string>?} forbiddenUserIds - don't send message to these users
+ */
+exports.sendMessageToGroup = async ({ groupId, message, notification, fetchedUsers, forbiddenUserIds }) => {
+  console.log('sendMessageToGroup', groupId, message, notification, fetchedUsers, forbiddenUserIds)
+  const users = await getGroupUsers({
+    groupId,
+    fetchedUsers,
+    forbiddenUserIds
+  })
+
+  console.log('sendMessageToGroup - create publish commands')
+  const publishSendMessageCommand = new PublishCommand({
+    TopicArn: SEND_MESSAGE_TOPIC_ARN,
+    Message: JSON.stringify({
+      users,
+      message
+    })
+  })
+
+  const publishSendNotificationCommand = new PublishCommand({
+    TopicArn: SEND_NOTIFICATION_TOPIC_ARN,
+    Message: JSON.stringify({
+      topic: `group-${groupId}`,
+      notification
+    })
+  })
+
+  console.log('sendMessageToGroup - return')
+  return Promise.allSettled([
+    snsClient.send(publishSendMessageCommand),
+    snsClient.send(publishSendNotificationCommand)
+  ])
+}
