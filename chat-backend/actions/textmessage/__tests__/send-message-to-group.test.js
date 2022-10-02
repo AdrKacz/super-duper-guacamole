1c07e78b46502bf71c51e14121d2e04431eb2472
AdrKacz <adrien.kaczmarek@gmail.com>
2022-10-02 12:04:41 +0100

    test: add coverage for text message send message to group

---
+++ chat-backend/actions/textmessage/__tests__/send-message-to-group.test.js
@@ -0,0 +1,67 @@
+// ===== ==== ====
+// IMPORTS
+const { sendMessageToGroup } = require('../src/send-message-to-group')
+const getGroupUsersModule = require('../src/get-group-users')
+const { mockClient } = require('aws-sdk-client-mock')
+
+const { SNSClient, PublishCommand } = require('@aws-sdk/client-sns')
+
+// ===== ==== ====
+// CONSTANTS
+const snsMock = mockClient(SNSClient)
+
+jest.mock('../src/get-group-users')
+
+const log = jest.spyOn(console, 'log').mockImplementation(() => {}) // skipcq: JS-0057
+
+// ===== ==== ====
+// BEFORE EACH
+beforeEach(() => {
+  // reset mocks
+  snsMock.reset()
+
+  snsMock.resolves({})
+
+  // reset console
+  log.mockReset()
+})
+
+test('it has environment variables', () => {
+  expect(process.env.SEND_MESSAGE_TOPIC_ARN).toBeDefined()
+  expect(process.env.SEND_NOTIFICATION_TOPIC_ARN).toBeDefined()
+})
+
+test('it sends message and notification', async () => {
+  const groupId = 'group-id'
+  const message = { message: 'message' }
+  const notification = { message: 'notification' }
+  const users = [{ id: 'user-a' }, { id: 'user-b' }, { id: 'user-c' }]
+  getGroupUsersModule.getGroupUsers.mockResolvedValue(users)
+
+  await sendMessageToGroup({
+    groupId,
+    message,
+    notification
+  })
+
+  expect(getGroupUsersModule.getGroupUsers).toHaveBeenCalledTimes(1)
+  expect(getGroupUsersModule.getGroupUsers).toHaveBeenCalledWith({ groupId })
+
+  expect(snsMock).toHaveReceivedCommandTimes(PublishCommand, 2)
+
+  expect(snsMock).toHaveReceivedCommandWith(PublishCommand, {
+    TopicArn: process.env.SEND_MESSAGE_TOPIC_ARN,
+    Message: JSON.stringify({
+      users,
+      message
+    })
+  })
+
+  expect(snsMock).toHaveReceivedCommandWith(PublishCommand, {
+    TopicArn: process.env.SEND_NOTIFICATION_TOPIC_ARN,
+    Message: JSON.stringify({
+      topic: `group-${groupId}`,
+      notification
+    })
+  })
+})
