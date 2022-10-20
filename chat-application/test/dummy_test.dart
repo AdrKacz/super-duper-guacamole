88a3f75aa7ba11ac9981489f6ac9d915a5d5bf82
AdrKacz <adrien.kaczmarek@gmail.com>
2022-10-20 17:49:41 +0100

    test: add sample test

--- chat-application/test/dummy_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-// ignore: unused_import
-// import 'package:awachat/main.dart';
-
-void main() {
-  test('dummy test', () {
-    expect(true, isTrue);
-  });
-}
