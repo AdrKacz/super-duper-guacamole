d9c1f967ddfd601370981167d169d8187b751e66
AdrKacz <adrien.kaczmarek@gmail.com>
2022-10-20 17:12:24 +0100

    ci: remove coverage

--- chat-application/test/dummy_test.dart
+++ chat-application/test/dummy_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 // ignore: unused_import
-import 'package:awachat/main.dart';
+// import 'package:awachat/main.dart';
 
 void main() {
   test('dummy test', () {
