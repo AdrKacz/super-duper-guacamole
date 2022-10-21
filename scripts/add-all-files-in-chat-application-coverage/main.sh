7f729830de75b93c3df9f5a30faed1f9e98fe740
AdrKacz <adrien.kaczmarek@gmail.com>
2022-10-21 18:09:15 +0100

    fix: update sh script

--- scripts/add-all-files-in-chat-application-coverage/main.sh
+++ scripts/add-all-files-in-chat-application-coverage/main.sh
@@ -1,6 +1,8 @@
 file=test/coverage_helper_test.dart
 echo "// More details: https://github.com/flutter/flutter/issues/27997"
-echo "// Helper file to make coverage work for all dart files\n" > $file
-echo "// ignore_for_file: unused_import" >> $file
-find lib -name '*.dart' | cut -c4- | awk -v package=$1 '{printf "import '\''package:%s%s'\'';\n", package, $1}' >> $file
-echo "\nvoid main(){}" >> $file
+{
+    printf "// Helper file to make coverage work for all dart files\n"
+    printf "// ignore_for_file: unused_import\n"
+    find lib -name '*.dart' | cut -c4- | awk -v package="$1" '{printf "import '\''package:%s%s'\'';\n", package, $1}'
+    printf "\nvoid main(){}"
+} > $file
