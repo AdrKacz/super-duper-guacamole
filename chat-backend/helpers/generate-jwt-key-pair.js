76a015c3f4265caa907791a4b14dedf51a5c9246
AdrKacz <adrien.kaczmarek@gmail.com>
2022-11-05 11:28:18 +0000

    feat: move private key to key store

--- chat-backend/helpers/generate-jwt-key-pair.js
+++ chat-backend/helpers/generate-jwt-key-pair.js
@@ -5,6 +5,15 @@ const {
 
 const fs = require('node:fs')
 
+const { argv } = require('node:process')
+
+let kid = null
+if (argv.length > 2) {
+  kid = argv[2]
+} else {
+  throw new Error('you must provide one parameter for kid (ex: yarn node generate-jwt-key-pair.js my-key')
+}
+
 const {
   publicKey,
   privateKey
@@ -25,18 +34,18 @@ console.log('privateKey', privateKey)
 
 const jwk = {
   ...publicKey,
-  kid: 'key',
+  kid,
   alg: 'RS256',
   use: 'sig'
 }
 
 console.log('jwk', jwk)
 
-fs.readFile('./jwks.json', (readErr, data) => {
+fs.readFile('./.well-known/jwks.json', (readErr, data) => {
   if (readErr) throw readErr
   const json = JSON.parse(data)
   json.keys.push(jwk)
-  fs.writeFile('./jwks.json', JSON.stringify(json, null, 2), (writeErr) => {
+  fs.writeFile('./.well-known/jwks.json', JSON.stringify(json, null, 2), (writeErr) => {
     if (writeErr) throw writeErr
     console.log('The "data to append" was appended to file!')
   })
