2d1bcf6151db898e1b5ca10af63608ddba5ef015
AdrKacz <adrien.kaczmarek@gmail.com>
2022-11-07 17:59:48 +0000

    feat: add get post sign in and sign up

--- chat-application/lib/network/http_connection.dart
+++ chat-application/lib/network/http_connection.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:io';
 import 'dart:typed_data';
 
 import 'package:awachat/pointycastle/helpers.dart';
@@ -32,35 +33,45 @@ class HttpConnection {
         body: jsonEncode(body));
   }
 
-  // Future<http.Response> signUp() {
-  //   return http.put(Uri.parse('$_httpEndpoint/sign-up'),
-  //       body: jsonEncode({
-  //         'id': User().id,
-  //         'publicKey': encodePublicKeyToPem(User().pair.publicKey)
-  //       }));
-  // }
+  Future<http.Response> signUp() {
+    return http.put(Uri.parse('$_httpEndpoint/sign-up'),
+        body: jsonEncode({
+          'id': User().id,
+          'publicKey': encodePublicKeyToPem(User().pair.publicKey)
+        }));
+  }
 
-  // Future<http.Response> signIn() {
-  //   int timestamp = DateTime.now().millisecondsSinceEpoch;
-  //   Uint8List signature = rsaSign(User().pair.privateKey,
-  //       Uint8List.fromList((User().id + timestamp.toString()).codeUnits));
+  Future<http.Response> signIn() {
+    int timestamp = DateTime.now().millisecondsSinceEpoch;
+    Uint8List signature = rsaSign(User().pair.privateKey,
+        Uint8List.fromList((User().id + timestamp.toString()).codeUnits));
 
-  //   return http.put(Uri.parse('$_httpEndpoint/sign-in'),
-  //       body: jsonEncode(
-  //           {'id': User().id, 'timestamp': timestamp, 'signature': signature}));
-  // }
+    return http.put(Uri.parse('$_httpEndpoint/sign-in'),
+        body: jsonEncode(
+            {'id': User().id, 'timestamp': timestamp, 'signature': signature}));
+  }
 
-  // Future<http.Response>? authenticatedPost(
-  //     {required String path,
-  //     required Map body,
-  //     bool useSignIn = false,
-  //     int numberOfRetries = 0}) {
-  //   if (numberOfRetries >= 3) {
-  //     return null;
-  //   }
-  //   // ignore: unnecessary_brace_in_string_interps
-  //   return http.put(Uri.parse('$_httpEndpoint/$path'), body: jsonEncode(body));
-  // }
+  Future<http.Response> get({required String path, int n = 0}) async {
+    http.Response response = await http.get(Uri.parse('$_httpEndpoint/$path'));
 
-  // Future<Map>
+    if (response.statusCode == 200 || n >= 3) {
+      return response;
+    } else {
+      await signIn();
+      return get(path: path, n: n + 1);
+    }
+  }
+
+  Future<http.Response> post(
+      {required String path, required Map body, int n = 0}) async {
+    http.Response response = await http.put(Uri.parse('$_httpEndpoint/$path'),
+        body: jsonEncode(body));
+
+    if (response.statusCode == 200 || n >= 3) {
+      return response;
+    } else {
+      await signIn();
+      return post(path: path, body: body, n: n + 1);
+    }
+  }
 }
