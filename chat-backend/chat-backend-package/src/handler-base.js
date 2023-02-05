98737273404d4bf23ae64af7922321f1041071ce
AdrKacz <adrien.kaczmarek@gmail.com>
2023-02-05 16:53:17 +0000

    fix: remove handler base

--- chat-backend/chat-backend-package/src/handler-base.js
+++
@@ -1,6 +0,0 @@
-exports.handlerBase = async ({ event, handler }) => {
-  console.log(JSON.stringify(event, null, 2))
-  const response = await handler(event)
-  console.log(JSON.stringify(response, null, 2))
-  return response
-}
