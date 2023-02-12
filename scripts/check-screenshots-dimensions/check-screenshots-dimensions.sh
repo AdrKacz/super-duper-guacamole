9df90b63043b98fa22b2c7b15ea7e16f5d33b0fc
AdrKacz <adrien.kaczmarek@gmail.com>
2023-02-12 19:53:31 +0000

    fix: update script

--- scripts/check-screenshots-dimensions/check-screenshots-dimensions.sh
+++ scripts/check-screenshots-dimensions/check-screenshots-dimensions.sh
@@ -12,31 +12,27 @@ do
   # Extract the suffix from the file name
   suffix=$(echo "$file" | cut -d _ -f2- | cut -d . -f1)
 
-  # Skip the file if it doesn't have a suffix
-  if [ "$suffix" = "$file" ]
-  then
-    continue
-  fi
-
   # Get the dimensions of the current image
   width=$(sips -g pixelWidth "$file" | awk '{print $2}')
   height=$(sips -g pixelHeight "$file" | awk '{print $2}')
 
   width=${width//$'\n'/}
-  height=${width//$'\n'/}
+  height=${height//$'\n'/}
   # If the dimensions of the first image for the current suffix have not been set yet, set them
   if [ -z "${suffix_info[$suffix]}" ]
   then
     suffix_info[$suffix]="$width x $height,1"
   else
     # Increment the number of images for the current suffix
+    dimensions=$(echo "${suffix_info[$suffix]}" | cut -d , -f1)
     count=$(echo "${suffix_info[$suffix]}" | cut -d , -f2)
     count=$((count + 1))
-    suffix_info[$suffix]="$width x $height,$count"
+    suffix_info[$suffix]="$dimensions,$count"
   fi
 
   # Compare the dimensions of the current image with the dimensions of the first image for the current suffix
-  if [ "$(echo "${suffix_info[$suffix]}" | cut -d , -f1)" != "$width x $height" ]
+  dimensions=$(echo "${suffix_info[$suffix]}" | cut -d , -f1)
+  if [ "$dimensions" != "$width x $height" ]
   then
     # If the dimensions are different, print an error message and exit the script
     echo "Error: Images with suffix $suffix in the folder have different dimensions."
