961e5c6b14d42a6f209fbe60102db008e5af6ddf
AdrKacz <61620013+AdrKacz@users.noreply.github.com>
2023-02-17 11:40:50 +0000

    366 save message being type when closing app (#368)
    * feat: toggle off keyboard on scroll
    
    * feat: save typed message in flyer chat
    
    * fix: update app version

--- chat-application/lib/widgets/chat/widgets/flyer_chat.dart
+++ chat-application/lib/widgets/chat/widgets/flyer_chat.dart
@@ -1,4 +1,5 @@
 import 'package:awachat/l10n/flyer_l10n.dart';
+import 'package:awachat/store/memory.dart';
 import 'package:awachat/store/user.dart';
 import 'package:awachat/widgets/chat/widgets/flyer_user_avatar.dart';
 import 'package:flutter/material.dart';
@@ -6,7 +7,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
 import 'package:flutter_chat_ui/flutter_chat_ui.dart';
 
-class FlyerChat extends StatelessWidget {
+class FlyerChat extends StatefulWidget {
   const FlyerChat(
       {Key? key,
       required this.messages,
@@ -19,6 +20,19 @@ class FlyerChat extends StatelessWidget {
   final void Function(BuildContext, types.Message)? onMessageLongPress;
 
   @override
+  State<FlyerChat> createState() => _FlyerChatState();
+}
+
+class _FlyerChatState extends State<FlyerChat> {
+  final InputTextFieldController _controller = InputTextFieldController();
+
+  @override
+  void initState() {
+    super.initState();
+    _controller.text = Memory().boxUser.get('typingMessage') ?? '';
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Chat(
         avatarBuilder: (String userId) => FlyerUserAvatar(userId: userId),
@@ -26,10 +40,19 @@ class FlyerChat extends StatelessWidget {
         showUserAvatars: true,
         textMessageOptions: const TextMessageOptions(isTextSelectable: false),
         l10n: const ChatL10nFr(),
-        messages: messages,
-        onSendPressed: onSendPressed,
-        onMessageLongPress: onMessageLongPress,
+        messages: widget.messages,
+        onSendPressed: (types.PartialText text) {
+          widget.onSendPressed(text);
+          Memory().boxUser.delete('typingMessage');
+        },
+        onMessageLongPress: widget.onMessageLongPress,
         user: types.User(id: User().id!),
+        keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+        inputOptions: InputOptions(
+            textEditingController: _controller,
+            onTextChanged: (String text) {
+              Memory().boxUser.put('typingMessage', text);
+            }),
         theme: DefaultChatTheme(
             primaryColor: Theme.of(context).colorScheme.onPrimary,
             inputBackgroundColor: Theme.of(context).colorScheme.primary,
