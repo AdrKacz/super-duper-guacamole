310ae16dd00d4861b1a8043b3b3fffdc764af271
AdrKacz <adrien.kaczmarek@gmail.com>
2023-03-02 07:50:21 +0000

    fix: update comment

--- chat-application/lib/store/user.dart
+++ chat-application/lib/store/user.dart
@@ -91,7 +91,7 @@ class User {
         Map? user = Memory().boxGroupUsers.get(groupUserKey) ?? {};
         Map groupUser = groupUsers[groupUserKey]!;
 
-        // TODO: need to force update if cannot retrieve the name or the photo
+        // TODO: need to force update if cannot retrieve the name or the photo from memory
         Map userData = await HttpConnection().post(
             path: 'download-user',
             body: {'id': groupUser['id'], 'lastUpdate': user['lastUpdate']});
