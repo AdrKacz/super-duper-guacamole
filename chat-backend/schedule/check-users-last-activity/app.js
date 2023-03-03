0cd1d437d4ecb4fe0505f389dadfafbb9a6e668a
AdrKacz <61620013+AdrKacz@users.noreply.github.com>
2023-03-03 08:23:06 +0000

    #418 Notifier si trop longtemps sans activité (#421)
    * feat: prepare package
    
    * fix: update deepsource config
    
    * feat: add scan to check users function
    
    * feat: notify users not here for more than a day
    
    * feat: add schedule to backend template
    
    * fix: update comments
    
    * fix: only query active group
    
    * fix: only query active groups
    
    * fix: update length group
    
    * fix: update for deepsource
    
    * test: test schedule
    
    * fix: remove TODO already done

---
+++ chat-backend/schedule/check-users-last-activity/app.js
@@ -0,0 +1,85 @@
+// ===== ==== ====
+// IMPORTS
+const { dynamoDBDocumentClient } = require('chat-backend-package/src/clients/aws/dynamo-db-client') // skipcq: JS-0260
+const { ScanCommand } = require('@aws-sdk/lib-dynamodb') // skipcq: JS-0260
+
+const { getGroup } = require('chat-backend-package/src/get-group') // skipcq: JS-0260
+const { sendNotifications } = require('chat-backend-package/src/send-notifications') // skipcq: JS-0260
+
+const { GROUPS_TABLE_NAME } = process.env
+
+const MILLISECONDS_PER_DAY = 86400000
+
+// ===== ==== ====
+// EXPORTS
+exports.handler = async (event) => {
+  console.log('Receives:', JSON.stringify(event, null, 2))
+
+  // scan all users
+  const groups = []
+  let previousLastEvaluatedKey = null
+  let hasReachLastPage = false
+  while (!hasReachLastPage) {
+    const scanCommandInputOptions = {
+      TableName: GROUPS_TABLE_NAME,
+      ProjectionExpression: '#id',
+      FilterExpression: '#isPublic = :true',
+      ExpressionAttributeNames: {
+        '#id': 'id',
+        '#isPublic': 'isPublic'
+      },
+      ExpressionAttributeValues: {
+        ':true': true
+      }
+    }
+
+    if (previousLastEvaluatedKey !== null) {
+      scanCommandInputOptions.ExclusiveStartKey = previousLastEvaluatedKey
+    }
+
+    const { Items: items, LastEvaluatedKey: lastEvaluatedKey } = await dynamoDBDocumentClient.send(new ScanCommand(scanCommandInputOptions))
+    groups.push(...items)
+
+    if (typeof lastEvaluatedKey === 'undefined') {
+      hasReachLastPage = true
+    } else {
+      previousLastEvaluatedKey = lastEvaluatedKey
+    }
+  }
+
+  console.log(`scanned ${groups.length} groups`, groups)
+
+  // look for users without activity
+  const todayString = (new Date()).toISOString().split('T')[0]
+  const today = new Date(todayString)
+  const usersWithoutActivity = []
+  console.log('start analysis for today', today)
+  for (const { id: groupId } of groups) {
+    try {
+      const { group, users } = await getGroup({ groupId })
+      console.log('analyse group', group, users)
+      for (const user of users) {
+        const id = user.id
+        const lastConnectionDayString = user.lastConnectionDay ?? 0
+        const lastConnectionDay = new Date(lastConnectionDayString)
+        const differenceInDay = Math.floor((today - lastConnectionDay) / MILLISECONDS_PER_DAY)
+        console.log(`user (${id}) difference in day is ${differenceInDay} (last connection day is ${lastConnectionDay})`)
+        if (differenceInDay > 0) {
+          console.log('too long before last activity, send notification to user', id)
+          usersWithoutActivity.push(user)
+        }
+      }
+    } catch (error) {
+      console.log(`error while getting group (${groupId})`, error)
+    }
+  }
+
+  // notify users
+  await sendNotifications({
+    users: usersWithoutActivity,
+    notification: {
+      title: 'Viens donner de tes nouvelles 🎉',
+      body: 'Ton groupe a besoin de toi !'
+    }
+  })
+}
